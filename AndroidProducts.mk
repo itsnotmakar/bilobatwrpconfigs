7d2b844b8f521fb22e325d4ae651814d7150e427
itsnotmakar <98771969+itsnotmakar@users.noreply.github.com>
2022-06-28 14:37:42 +0300

    Add files via upload
    тврп убивает

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_biloba.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_biloba-user \
+    twrp_biloba-userdebug \
+    twrp_biloba-eng
